f2377e9d1335abdec5eef23768a2fd687b6977a6
shagun-abjima <153979823+shagun-abjima@users.noreply.github.com>
2024-01-18 18:33:16 +0530

    initial commit

---
+++ Food_App/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected=new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('Arrabiata pasta', 'Recipe will be displayed here.', 'https://www.kitchensanctuary.com/wp-content/uploads/2023/07/Penne-alarrabiata-square-FS.jpg'),
+    new Recipe('Alfredo pasta', 'Recipe will be displayed here.', 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/k%2FPhoto%2FRecipes%2F2023-06-chicken-alfredo-pasta-reshoot%2Fchicken-alfredo-pasta-2-vertical')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onRecipeSelected(recipe:Recipe){
+    this.recipeWasSelected.emit(recipe);
+
+  }
+
+}
